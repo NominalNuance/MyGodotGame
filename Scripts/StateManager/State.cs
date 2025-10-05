943f21bbc8ad399aa91440f74de0795814d337e1
NominalNuance <rattesque@gmail.com>
2025-10-05 15:09:28 -0700

    Finished state script. Potential cleanup later?

--- Scripts/StateManager/State.cs
+++ Scripts/StateManager/State.cs
@@ -1,7 +1,7 @@
 
 using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
 
 public class State(string newName, object newValue)
 {
@@ -9,7 +9,7 @@ public class State(string newName, object newValue)
     public object CurrentState { get; private set; } = newValue;
     public object DefaultState { get; private set; } = newValue;
     public Type StateType { get; private set; } = newValue.GetType();
-    public Dictionary<WeakReference, Dictionary<Func<object>, Func<object, bool>>> Listeners = [];
+    public ConditionalWeakTable<object, List<ListenerPayload>> Listeners = [];
 
     public State UpdateState(object newValue) 
     {
@@ -32,25 +32,47 @@ public class State(string newName, object newValue)
 
     public void EmitStateUpdate() 
     {
-
+        foreach (var (listener, payloadList) in Listeners) 
+        {
+            foreach (var payload in payloadList)
+            {
+                if (payload.Conditional(CurrentState))
+                {
+                    payload.ListenerFunction(CurrentState);
+                }
+            }
+        }
     }
 
-    public void AddListener(object listener, Func<object> listenerFunction, Func<object, bool> conditional = null)
+    public void AddListener(object listener, Action<object> listenerFunction, Func<object, bool> conditional = null)
     {
         // ??= is the Null-Coalescing Assignment Operator. It checks if the left side is null
         // If so, it assigns to that null value the value on the right side.
         conditional ??= (object obj) => true;
 
-        WeakReference weak_listener = new(listener);
-
-        //If the listener is not in the Listener Dictionary, then we add them to the dictionary with a new and empty inner_dict
-        if (!Listeners.TryGetValue(weak_listener, out var inner_dict))
+        //If the listener is not in the Listener Dictionary, then we add them to the dictionary with a new and empty payload_list
+        if (!Listeners.TryGetValue(listener, out var payload_list))
         {
-            inner_dict = [];
-            Listeners[weak_listener] = inner_dict;
+            payload_list = [];
+            Listeners.Add(listener, payload_list);
         }
 
-        inner_dict[listenerFunction] = conditional;
+        payload_list.Add( new ListenerPayload(listenerFunction, conditional));
 
     }
+
+    //uses only the listener. This removes then entire listener from the subscribers and not just a specific function
+    //this may be fine depending on how the project develops, keep this mind incase we ever need to just
+    //unsubscribe specific functions for a listener without completely unsubscribing the listener
+    public void RemoveListener(object listener) 
+    {
+        Listeners.Remove(listener);
+    }
+}
+
+public class ListenerPayload(Action<object> newListenerFunction, Func<object, bool> newConditional)
+{
+    public Action<object> ListenerFunction { get; private set; } = newListenerFunction;
+    public Func<object, bool> Conditional { get; private set; } = newConditional ?? ((object obj) => true);
+
 }
