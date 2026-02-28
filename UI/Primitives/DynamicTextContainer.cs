39af9fb3f69c8645074df4390fa7f814af7bdf5e
NominalNuance <rattesque@gmail.com>
2026-02-28 14:02:18 -0800

    Worked on StatCounter and DynamicTextContainer some more.

--- UI/Primitives/DynamicTextContainer.cs
+++ UI/Primitives/DynamicTextContainer.cs
@@ -3,13 +3,46 @@ using System;
 
 public partial class DynamicTextContainer : Control
 {
-    [Export] public string Content;
-    [Export] public Texture Image;
-    [Export] public string BundleName;
-    [Export] public string StateName;
+    private string _content;
+    [Export] public string Content
+    {
+        get { return _content; }
+        set 
+        { 
+            _content = value; 
+            if (ThisLabel != null) ThisLabel.Text = value;
+        }
+    }
+
+    private Texture2D _image;
+    [Export] public Texture2D Image
+    {
+        get { return _image; }
+        set 
+        { 
+            _image = value; 
+            if (ThisTextureRect != null) ThisTextureRect.Texture = value;
+        }
+    }
+
+    [Export] private string BundleName;
+    [Export] private string StateName;
+
+    private Label ThisLabel;
+    private TextureRect ThisTextureRect;
+    public StatCounter ThisStatCounter;
 
     public override void _Ready()
     {
+        ThisLabel = GetNode<Label>("%Label");
+        ThisTextureRect = GetNode<TextureRect>("%TextureRect");
+        ThisStatCounter = GetNode<StatCounter>("%StatCounter");
 
+        Content = _content;
+        Image = _image;
+
+        ThisStatCounter.ChangeSubscribedState(BundleName, StateName);
+
+        ThisLabel.Text = "Test";
     }
 }
--- UI/Primitives/StatCounter.cs
+++ UI/Primitives/StatCounter.cs
@@ -5,14 +5,25 @@ using System;
 public partial class StatCounter : Label
 {
     private StateManager SM;
-    [Export] public string BundleName;
+
+    private (string BundleName, string StateName) TargetState {get; set;}
+    [Export] public string BundleName { get;  set;}
     [Export] public string StateName;
 
     public override void _Ready()
     {
         SM = GetNode<StateManager>("/root/StateManager");
-        SM.Subscribe(BundleName, StateName, this, UpdateCounter);
+        TargetState = (BundleName, StateName);
+        
+        SM.Subscribe(TargetState.BundleName, TargetState.StateName, this, UpdateCounter);
         Text = "Pingas";
+    }
+
+    public void ChangeSubscribedState (string newBundleName, string newStateName)
+    {
+        SM.Unsubscribe(TargetState.BundleName, TargetState.StateName, this);
+        TargetState = (newBundleName, newStateName);
+        SM.Subscribe(TargetState.BundleName, TargetState.StateName, this, UpdateCounter);
     }
     private void UpdateCounter(object newUpdate)
     {
