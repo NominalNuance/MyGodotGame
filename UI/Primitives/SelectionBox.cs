d1167c36bfb0dad6726e00c33515b5db74cd4065
NominalNuance <rattesque@gmail.com>
2026-03-21 21:03:39 -0700

    Made a very bad UI script

--- UI/Primitives/SelectionBox.cs
+++ UI/Primitives/SelectionBox.cs
@@ -14,6 +14,21 @@ public partial class SelectionBox : Control
         ThisLabel.MouseEntered += LabelMoused;
         ThisLabel.FocusEntered += LabelFocused;
 
+        ThisLabel2 = GetNode<Label>("%Label2");
+        ThisLabel2.FocusMode = FocusModeEnum.All;
+        ThisLabel2.MouseEntered += LabelMoused2;
+        ThisLabel2.FocusEntered += LabelFocused2;
+
+        ThisLabel3 = GetNode<Label>("%Label3");
+        ThisLabel3.FocusMode = FocusModeEnum.All;
+        ThisLabel3.MouseEntered += LabelMoused3;
+        ThisLabel3.FocusEntered += LabelFocused3;
+
+        ThisLabel4 = GetNode<Label>("%Label4");
+        ThisLabel4.FocusMode = FocusModeEnum.All;
+        ThisLabel4.MouseEntered += LabelMoused4;
+        ThisLabel4.FocusEntered += LabelFocused4;
+
         ThisLabel.GrabFocus();
         
     }
@@ -27,5 +42,38 @@ public partial class SelectionBox : Control
     {
         ThisLabel.GrabFocus();
         GD.Print("Option 1 has been focused");
+    }
+
+    private void LabelMoused2()
+    {
+        ThisLabel2.GrabFocus();
+        GD.Print("Option 2 focused by mouse");
+    }
+    private void LabelFocused2()
+    {
+        ThisLabel2.GrabFocus();
+        GD.Print("Option 2 has been focused");
+    }
+
+    private void LabelMoused3()
+    {
+        ThisLabel3.GrabFocus();
+        GD.Print("Option 3 focused by mouse");
+    }
+    private void LabelFocused3()
+    {
+        ThisLabel3.GrabFocus();
+        GD.Print("Option 3 has been focused");
+    }
+
+    private void LabelMoused4()
+    {
+        ThisLabel4.GrabFocus();
+        GD.Print("Option 4 focused by mouse");
+    }
+    private void LabelFocused4()
+    {
+        ThisLabel4.GrabFocus();
+        GD.Print("Option 4 has been focused");
     }
 }
